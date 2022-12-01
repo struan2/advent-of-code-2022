5f6d1b386354dd6bc92f077a611a5f85070420b7
Struan Robertson <struan.robertson@freeagent.com>
2022-12-01 08:36:37 +0000

    Add Day 1 plus Setup

---
+++ day1.rb
@@ -0,0 +1,26 @@
+class Day1
+  def initialize(input_file)
+    @input_file = input_file
+  end
+
+  def part1
+    sumed_data.max
+  end
+
+  def part2
+    sumed_data.max(3).reduce(:+)
+  end
+
+  private
+
+  attr_reader :input_file
+
+  def file_data
+    raw_data = File.read(input_file)
+    split_data = raw_data.split("\n\n").map{ |e| e.split("\n").map(&:to_i) }
+  end
+
+  def sumed_data
+    file_data.map{ |e| e.reduce(:+) }
+  end
+end
---
+++ spec/day1_spec.rb
@@ -0,0 +1,26 @@
+require_relative "../day1"
+
+RSpec.describe Day1 do
+  subject(:day1_test) { Day1.new("spec/fixtures/day1_test.txt") }
+  subject(:day1_actual) { Day1.new("spec/fixtures/day1_actual.txt") }
+
+  describe "#part1" do
+    it "returns the correct value" do
+      expect(day1_test.part1).to eq(24000)
+    end
+
+    it "returns the correct value" do
+      expect(day1_actual.part1).to eq(69795)
+    end
+  end
+
+  describe "#part2" do
+    it "returns the correct value" do
+      expect(day1_test.part2).to eq(45000)
+    end
+
+    it "returns the correct value" do
+      expect(day1_actual.part2).to eq(208437)
+    end
+  end
+end
